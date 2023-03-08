1148b1c9171d9c252518e63bfadf47295a56df61
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-08 14:52:44 +0200

    linked between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,15 @@
 // ourDomain.com/news
 
+import Link from "next/link";
+
 function NewsPage(){
-    return <h1>News Page</h1>
+    return <>
+        <h1>News Page</h1>
+        <ul>
+            <li><Link href="/news/nextjs-is-a-great-framework">NextJs is a great framework</Link></li>
+            <li><Link href="/news/something-else">Something else</Link></li>
+        </ul>
+        </>
 }
 
 export default NewsPage
