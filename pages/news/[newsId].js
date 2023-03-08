9cc396fc8bcc6828d223e141fbcd630121ad173a
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-08 14:37:17 +0200

    extracted the dynamic route data

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,13 @@
 // ourDomain.com/news/detailed-news
 
+import {useRouter} from "next/router"
+
 function DetailedNews(){
+
+    const router = useRouter();
+
+    console.log(router.query.newsId);
+
     return <h1>Specific news</h1>
 }
 
